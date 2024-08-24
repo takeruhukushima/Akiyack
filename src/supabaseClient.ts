2eb9573c0165b93be54935dd85fb1d643628253f
takeru.fukushima <100330935+takeruhukushima@users.noreply.github.com>
2024-08-24 13:20:15 +0900

    Add .env to .gitignore 2

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_KEY!;
+const supabaseUrl = process.env.React_APP_SUPABASE_URL!;
+const supabaseKey = process.env.React_APP_SUPABASE_KEY!;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
